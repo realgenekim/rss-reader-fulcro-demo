866e706daecbbc281e0342acb6935dfca6935a7d
Gene Kim <genek@itrevolution.com>
2023-03-01 13:57:44 -0800

    first useful test

--- test/genek/core_test.clj
+++ test/genek/core_test.clj
@@ -1,9 +1,23 @@
 (ns genek.core-test
   (:require
     [clojure.test :as t :refer :all]
+    [com.example.membrane-ui.client :as client]
     [clojure.spec.alpha :as s]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
+(deftest client-http
+
+  (let [app (client/create-client)]
+
+    (def app app)
+
+    (is (fn?
+          (-> app :com.fulcrologic.fulcro.application/runtime-atom deref :com.fulcrologic.fulcro.application/remotes
+            :remote :transmit!))))
+
+
+
+  0)
